c45f3bc08964eb16fe57cdfae6d24156b2fcc486
Su Ngo <you@example.com>
2025-06-02 07:36:09 +0700

    connect frontend, update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,9 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the entire app directory
-COPY ./app .
+COPY ./app ./app
+
+RUN pwd
 
 RUN ls
 
@@ -27,5 +29,5 @@ RUN ls
 EXPOSE 8000
 
 # Run the FastAPI app
-# CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-from database.database import Base, engine, DATABASE_URL
-from api.register import router
+from app.database.database import Base, engine, DATABASE_URL
+from app.api.register import router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 # @app.get("/")
 # async def root():
-#     return {"message": "Hello S3"}
+#     return {"message": "Hello Ss3"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router, prefix="/api")
