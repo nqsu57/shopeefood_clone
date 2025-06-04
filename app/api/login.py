146db92ee6398aba5d8d6acb1bf6e33e25a9072e
Su Ngo <quocsungo2000@gmail.com>
2025-06-04 17:27:41 +0700

    update notify login

--- app/api/login.py
+++ app/api/login.py
@@ -18,6 +18,6 @@ def get_db():
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == form_data.username).first()
     if not user or not verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail="Sai tài khoản hoặc mật khẩu")
+        raise HTTPException(status_code=401, detail="Incorrect username or password!")
     token = create_access_token({"sub": user.email})
     return {"access_token": token, "token_type": "bearer"}
