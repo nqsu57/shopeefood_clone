fccdab6fd069a693230ef9db71b8a4a80b5e06b0
Su Ngo <quocsungo2000@gmail.com>
2025-07-11 17:24:21 +0700

    relationship order and restaurant

--- app/model/order.py
+++ app/model/order.py
@@ -1,25 +1,34 @@
+import uuid
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime, Table
 from sqlalchemy.orm import relationship
 from app.database.database import Base
 from sqlalchemy.sql import func
 
+
+def generate_order_id():
+    return str(uuid.uuid4()).replace("-", "").upper()[:12]
+
+
 class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
+    orderID = Column(String(20), unique=True, index=True, nullable=False, default=generate_order_id)    
     payment_method = Column(String, default="COD")
     shipping_fee = Column(Integer, default=0)
     total = Column(Float, default=0)
     status = Column(String, default="pending")
-
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     address_id = Column(Integer, ForeignKey("addresses.id"))
     user_id = Column(Integer, ForeignKey("users.id"))
+    restaurant_id = Column(Integer, ForeignKey("restaurants.id"), nullable=False)
 
 
     user = relationship("User")
     items = relationship("OrderItem", back_populates="order", cascade="all, delete")
     address = relationship("Address", back_populates="orders")
+    restaurant = relationship("Restaurant")
 
 order_item_topping_table = Table(
     'order_item_toppings',
